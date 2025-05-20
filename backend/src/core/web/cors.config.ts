cafd12d1285bf2be3b3e7249cf3268d3672463f5
Houssein <hcamara903@gmail.com>
2025-05-20 21:38:23 +0200

    deploie

--- backend/src/core/web/cors.config.ts
+++ backend/src/core/web/cors.config.ts
@@ -1,14 +1,14 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { ConfigService } from '@nestjs/config';
 
+
 export const createCorsConfig = (
   configService: ConfigService,
 ): CorsOptions => ({
-  origin: configService.get<string>(
-    'ALLOWED_ORIGIN_URL',
-    'http://localhost:4200',
-  ),
+  origin: '*',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   preflightContinue: false,
   optionsSuccessStatus: 204,
 });
+
+
